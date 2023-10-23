3c7af872d37653f55ae652f2b5b9abeeaf0e625b
Mark Kavalecz <kiskav12@gmail.com>
2023-10-23 13:41:16 +0200

    added scanner method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,65 +1,64 @@
 package org.example;
 
-import java.io.InputStream;
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 public class Main {
+
+    private static UserInput userInput;
+
     private static void getUserName() {
         System.out.println("Üdvözlünk a Wumpus játékban!\nÍrd be a felhasználóneved: ");
-        Scanner sc=new Scanner(System.in);
-        String userName= sc.nextLine();
-        System.out.println("Köszönjük! További jó szórakozást "+userName+"!");
+        userInput.getUserInputAsString();
+        System.out.println("Köszönjük! További jó szórakozást!");
         mainMenu();
     }
+
     private static void mainMenu() {
         System.out.println("-------FŐMENÜ-------");
         System.out.println("1. JÁTÉK");
         System.out.println("2. PÁLYASZERKESZTÉS");
         System.out.println("3. KILÉPÉS");
-        Scanner sc=new Scanner(System.in);
-        int menuNumber=sc.nextInt();
-        switch (menuNumber)
-        {
-            case 1:game();
-            break;
-            case 2:mapEditor();
-            break;
-            case 3:exitGame();
-            break;
+        switch (userInput.getUserInputAsInt()) {
+            case 1:
+                game();
+                break;
+            case 2:
+                mapEditor();
+                break;
+            case 3:
+                exitGame();
+                break;
             default: {
                 System.out.println("NEM LÉTEZIK ILYEN PARANCS");
-                mainMenu();}
+                mainMenu();
+            }
         }
     }
+
     private static void game() {
         System.out.println("-------JÁTÉK-------");
         System.out.println("1. PÁLYA BETÖLTÉSE JÁTÉKBÓL");
         System.out.println("2. PÁLYA BETÖLTÉSE ADATBÁZISBÓL");
         System.out.println("3. VISSZA A FŐMENÜBE");
-        Scanner sc=new Scanner(System.in);
-        int menuNumber=sc.nextInt();
-        switch (menuNumber)
-        {
-            case 3:mainMenu();
-            default:{
+        switch (userInput.getUserInputAsInt()) {
+            case 3:
+                mainMenu();
+                break;
+            default: {
                 System.out.println("#cominsoon or #wrongcommand");
                 game();
             }
         }
     }
+
     private static void mapEditor() {
         System.out.println("-------PÁLYASZERKESZTŐ-------");
         System.out.println("1. PÁLYA LÉTREHOZÁSA    #comingsoon");
         System.out.println("2. PÁLYA BETÖLTÉSE    #comingsoon");
         System.out.println("3. PÁLYA MENTÉSE    #comingsoon");
         System.out.println("4. VISSZA A FŐMENÜBE");
-        Scanner sc=new Scanner(System.in);
-        int menuNumber=sc.nextInt();
-        switch (menuNumber)
-        {
-            case 4:mainMenu();
-            default:{
+        switch (userInput.getUserInputAsInt()) {
+            case 4:
+                mainMenu();
+            default: {
                 System.out.println("#cominsoon or #wrongcommand");
                 mapEditor();
             }
@@ -69,7 +68,10 @@ public class Main {
     private static void exitGame() {
         System.exit(0);
     }
+
     public static void main(String[] args) {
+        userInput = new UserInput();
         getUserName();
+
     }
 }
